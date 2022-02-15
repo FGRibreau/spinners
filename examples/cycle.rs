63ccbacc019d24ea45a0738116cd393f681fa110
jamesinaxx <grassojames5@gmail.com>
2022-02-15 16:30:20 +1100

    fixed some grammar

--- examples/cycle.rs
+++ examples/cycle.rs
@@ -3,7 +3,7 @@ use std::{thread::sleep, time::Duration};
 use strum::IntoEnumIterator;
 
 fn main() {
-    // loop through each spinner and display them during 2 seconds
+    // loop through each spinner and display them forc 2 seconds
     for spinner in Spinners::iter() {
         let sp = Spinner::new(spinner.clone(), format!("{:?}", spinner));
         sleep(Duration::from_secs(2));
